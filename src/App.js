ead432912d4e7252f1e06a22e7a845c1305a9288
QaziTalha4595 <97889460+QaziTalha4595@users.noreply.github.com>
2022-08-30 11:49:40 +0530

    modified:   package-lock.json 	modified:   package.json 	new file:   public/LICENSE 	new file:   public/README.md 	new file:   public/about.html 	new file:   public/contact.html 	new file:   public/css/clean-blog.css 	new file:   public/css/clean-blog.min.css 	new file:   public/gulpfile.js 	new file:   public/img/about-bg.jpg 	new file:   public/img/contact-bg.jpg 	new file:   public/img/home-bg.jpg 	new file:   public/img/post-bg.jpg 	new file:   public/img/post-sample-image.jpg 	modified:   public/index.html 	new file:   public/js/clean-blog.js 	new file:   public/js/clean-blog.min.js 	new file:   public/js/contact_me.js 	new file:   public/js/jqBootstrapValidation.js 	new file:   public/less/clean-blog.less 	new file:   public/less/mixins.less 	new file:   public/less/variables.less 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	new file:   public/mail/contact_me.php 	new file:   public/package.json 	new file:   public/post.html 	new file:   public/vendor/bootstrap/css/bootstrap.css 	new file:   public/vendor/bootstrap/css/bootstrap.min.css 	new file:   public/vendor/bootstrap/fonts/glyphicons-halflings-regular.eot 	new file:   public/vendor/bootstrap/fonts/glyphicons-halflings-regular.svg 	new file:   public/vendor/bootstrap/fonts/glyphicons-halflings-regular.ttf 	new file:   public/vendor/bootstrap/fonts/glyphicons-halflings-regular.woff 	new file:   public/vendor/bootstrap/fonts/glyphicons-halflings-regular.woff2 	new file:   public/vendor/bootstrap/js/bootstrap.js 	new file:   public/vendor/bootstrap/js/bootstrap.min.js 	new file:   public/vendor/font-awesome/css/font-awesome.css 	new file:   public/vendor/font-awesome/css/font-awesome.min.css 	new file:   public/vendor/font-awesome/fonts/FontAwesome.otf 	new file:   public/vendor/font-awesome/fonts/fontawesome-webfont.eot 	new file:   public/vendor/font-awesome/fonts/fontawesome-webfont.svg 	new file:   public/vendor/font-awesome/fonts/fontawesome-webfont.ttf 	new file:   public/vendor/font-awesome/fonts/fontawesome-webfont.woff 	new file:   public/vendor/font-awesome/fonts/fontawesome-webfont.woff2 	new file:   public/vendor/font-awesome/less/animated.less 	new file:   public/vendor/font-awesome/less/bordered-pulled.less 	new file:   public/vendor/font-awesome/less/core.less 	new file:   public/vendor/font-awesome/less/fixed-width.less 	new file:   public/vendor/font-awesome/less/font-awesome.less 	new file:   public/vendor/font-awesome/less/icons.less 	new file:   public/vendor/font-awesome/less/larger.less 	new file:   public/vendor/font-awesome/less/list.less 	new file:   public/vendor/font-awesome/less/mixins.less 	new file:   public/vendor/font-awesome/less/path.less 	new file:   public/vendor/font-awesome/less/rotated-flipped.less 	new file:   public/vendor/font-awesome/less/screen-reader.less 	new file:   public/vendor/font-awesome/less/stacked.less 	new file:   public/vendor/font-awesome/less/variables.less 	new file:   public/vendor/font-awesome/scss/_animated.scss 	new file:   public/vendor/font-awesome/scss/_bordered-pulled.scss 	new file:   public/vendor/font-awesome/scss/_core.scss 	new file:   public/vendor/font-awesome/scss/_fixed-width.scss 	new file:   public/vendor/font-awesome/scss/_icons.scss 	new file:   public/vendor/font-awesome/scss/_larger.scss 	new file:   public/vendor/font-awesome/scss/_list.scss 	new file:   public/vendor/font-awesome/scss/_mixins.scss 	new file:   public/vendor/font-awesome/scss/_path.scss 	new file:   public/vendor/font-awesome/scss/_rotated-flipped.scss 	new file:   public/vendor/font-awesome/scss/_screen-reader.scss 	new file:   public/vendor/font-awesome/scss/_stacked.scss 	new file:   public/vendor/font-awesome/scss/_variables.scss 	new file:   public/vendor/font-awesome/scss/font-awesome.scss 	new file:   public/vendor/jquery/jquery.js 	new file:   public/vendor/jquery/jquery.min.js 	modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes,Link } from 'react-router-dom'
+  export default function App() {
+  return ( 
+    <Router>
+<div>
+{/* <!-- Navigation --> */}
+    <nav className="navbar navbar-default navbar-custom navbar-fixed-top">
+        <div className="container-fluid">
+            {/* <!-- Brand and toggle get grouped for better mobile display --> */}
+            <div className="navbar-header page-scroll">
+                <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span className="sr-only">Toggle navigation</span>
+                    Menu <i className="fa fa-bars"></i>
+                </button>
+                <a className="navbar-brand" href="index.html">Start Bootstrap</a>
+            </div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-      First React App
-        </p>
-    
-      </header>
+            {/* <!-- Collect the nav links, forms, and other content for toggling --> */}
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul className="nav navbar-nav navbar-right">
+                    <li>
+                      <Link to="/">Home</Link>
+                        {/* <a href="">Home</a> */}
+                    </li>
+                    <li>
+                        {/* <a href="about.html">About</a> */}
+                        <Link to="about">About</Link>
+                    </li>
+                    <li>
+                        <a href="post.html">Sample Post</a>
+                    </li>
+                    <li>
+                        {/* <a href="contact.html">Contact</a> */}
+                        <Link to="users">Contact</Link>
+                    </li>
+                </ul>
+            </div>
+            {/* <!-- /.navbar-collapse --> */}
+        </div>
+        {/* <!-- /.container --> */}
+    </nav>
+
+  
+
+    {/* <!-- Main Content --> */}
+    <Routes>
+          <Route path="/about" element={<About/>}/> 
+          <Route path="/users" element={<Users/>}/> 
+          <Route path="/" element={<Home/>}/> 
+        </Routes>
+
+    <hr/>
+
+    {/* <!-- Footer --> */}
+    <footer>
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <ul className="list-inline text-center">
+                        <li>
+                            <a href="/">
+                                <span className="fa-stack fa-lg">
+                                    <i className="fa fa-circle fa-stack-2x"></i>
+                                    <i className="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/">
+                                <span className="fa-stack fa-lg">
+                                    <i className="fa fa-circle fa-stack-2x"></i>
+                                    <i className="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/">
+                                <span className="fa-stack fa-lg">
+                                    <i className="fa fa-circle fa-stack-2x"></i>
+                                    <i className="fa fa-github fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                    <p className="copyright text-muted">Copyright &copy; Your Website 2016</p>
+                </div>
+            </div>
+        </div>
+    </footer>
     </div>
+    </Router>
   );
 }
+function Home() {
+    return <div>
+  {/* <!-- Page Header --> */}
+    {/* <!-- Set your background image for this header on the line below. --> */}
+    <header className="intro-header" style={{backgroundImage: "url('img/home-bg.jpg')"}}>
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <div className="site-heading">
+                        <h1>Clean Blog</h1>
+                        <hr className="small"/>
+                        <span className="subheading">A Clean Blog Theme by Start Bootstrap</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
 
-export default App;
+    </div>;
+  }
+  
+  function About() {
+    return <h2>About</h2>;
+  }
+  
+  function Users() {
+    return <h2>Users</h2>;
+  }
